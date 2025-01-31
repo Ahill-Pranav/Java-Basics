573e4e9007300695c66131077e4197c971aa0213
Ahill Pranav M <itisahill.m@gmail.com>
2025-01-31 14:35:09 +0530

    .

---
+++ Day1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Day1
+{
+    public static void main(String[] args)
+    {
+        Scanner inp= new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name= inp.nextLine();
+        System.out.println("Hi "+name);
+    }
+}
---
+++ First_Q.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class First_Q {
+    public static void main(String[] args)
+    {
+        Scanner inp = new Scanner(System.in);
+        float input = inp.nextFloat();
+        System.out.printf("%.2f",input);
+    }
+}
+
